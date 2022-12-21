2f9482ce6435fad58a44abeba26596b7539b0cac
amoubasher <amoubasher@gmail.com>
2022-12-20 18:34:38 -0800

    Changed some logic to the apiRoutes

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
+// const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const util = require('util');
+
+const notes = require('../db/db.json');
 
 router.get('/', (req,res) => {
     // get all the notes
-    res.json(`GOT YOUR ${req.method} REQUEST}`)
+    // res.json(`GOT YOUR ${req.method} REQUEST}`)
+//read from db, respond with fresh data
+//const dataString = fs.readfileSync("../db/db.json", "utf-8")
+//const data = JSON.parse(dataString)
+    res.json(notes)
+
+
 })
 
 router.post('/', (req,res) => {
     res.json(`GOT YOUR ${req.method} REQUEST}`)
-})
+    console.log(req.body)
+    notes.push(req.body);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+}
+)
 
 module.exports = router;
